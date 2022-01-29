0b895c1de999e56b6c71fb5a85191ae6c423dda7
a-jwc <chauajw@gmail.com>
2022-01-28 18:27:58 -0800

    remove users endpoints

--- src/lib.rs
+++ src/lib.rs
@@ -97,7 +97,7 @@ pub async fn main(req: Request, env: Env) -> Result<Response> {
             let keys = kv.list().execute().await?.keys;
             let mut posts: Vec<Value> = vec![];
             for key in keys {
-                let mut value = kv.get(&key.name).await.unwrap().unwrap().as_string();
+                let value = kv.get(&key.name).await.unwrap().unwrap().as_string();
                 let j = json!(value);
                 posts.push(j);
             }
@@ -139,7 +139,7 @@ pub async fn main(req: Request, env: Env) -> Result<Response> {
                     console_log!("This is the req cookie 🍪: {}", req_cookie);
                     let client = reqwest::Client::new();
                     let auth_resp = client
-                        .get("https://ricky-division-score-chain.trycloudflare.com/verify")
+                        .get("https://tracked-specialized-webcam-mounts.trycloudflare.com/verify")
                         .header(COOKIE, req_cookie)
                         .send()
                         .await
@@ -156,7 +156,7 @@ pub async fn main(req: Request, env: Env) -> Result<Response> {
 
                 // * Get the set-cookie header from authorization server
                 let auth_resp = reqwest::get(format!(
-                    "https://ricky-division-score-chain.trycloudflare.com/auth/{}",
+                    "https://tracked-specialized-webcam-mounts.trycloudflare.com/auth/{}",
                     username
                 ))
                 .await
@@ -231,51 +231,6 @@ pub async fn main(req: Request, env: Env) -> Result<Response> {
                 "GET,HEAD,POST,OPTIONS",
             )?;
             Headers::set(headers, "Access-Control-Allow-Headers", "Content-Type")?;
-            Ok(res)
-        })
-        .get_async("/users", |_, ctx| async move {
-            let kv = ctx.kv("users")?;
-            let keys = kv.list().execute().await?.keys;
-            let mut users = vec![];
-            for key in keys {
-                users.push(key.name);
-            }
-            console_log!("{:#?}", users);
-            let mut res = Response::from_json(&users)?;
-            let headers = Response::headers_mut(&mut res);
-            Headers::set(
-                headers,
-                "Access-Control-Allow-Origin",
-                "https://cf-social-media-frontend.pages.dev",
-            )?;
-            Headers::set(headers, "transfer-encoding", "chunked")?;
-            Headers::set(headers, "vary", "Accept-Encoding")?;
-            Headers::set(headers, "connection", "keep-alive")?;
-            Ok(res)
-        })
-        .post_async("/users", |mut req, ctx| async move {
-            let mut new_user: Value = req.json::<serde_json::Value>().await?;
-            let now = Utc::now().to_rfc3339().to_string();
-            let new_user_obj = new_user.as_object_mut().unwrap();
-            let mut username = new_user_obj.get("username").unwrap().to_string();
-            username.pop();
-            username.remove(0);
-            let kv = ctx.kv("users")?;
-            kv.put(&username, &now)?.execute().await?;
-            let mut res = Response::ok(format!("{}", new_user))?;
-            let headers = Response::headers_mut(&mut res);
-            Headers::set(
-                headers,
-                "Access-Control-Allow-Origin",
-                "https://cf-social-media-frontend.pages.dev",
-            )?;
-            Headers::set(
-                headers,
-                "Access-Control-Allow-Methods",
-                "GET,HEAD,POST,OPTIONS",
-            )?;
-            Headers::set(headers, "Access-Control-Allow-Headers", "Content-Type")?;
-            Headers::set(headers, "Allow", "GET,HEAD,POST,OPTIONS")?;
             Ok(res)
         })
         .run(req, env)
